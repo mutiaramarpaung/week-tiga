9ec74f6994de20267fb85b38c0b20a61785de6d6
mutiaramarpaung <62175745+mutiaramarpaung@users.noreply.github.com>
2020-04-06 07:03:11 +0700

    Update builtInFunctionArray.js

--- builtInFunctionArray.js
+++ builtInFunctionArray.js
@@ -1,16 +1,34 @@
-/** built in function exercise 4 */
-function dataHandling2(parameter) {
-    parameter.splice(1,2, "Roman Alamsyah Elsharawy", "Provinsi Bandar Lampung" );
-    parameter.splice(4,2,"Pria", "SMA Internasional Metro");
-    console.log(parameter);
-    var tahun = parameter.slice(3,4);
-    var tahun_pisah= tahun.toString();
-    var tahunPisah = tahun_pisah.split("/");
-    var tahunn= tahunPisah.splice(0,3,"1989", "21", "05");
-    tahunPisah.splice(2,1,"5");
-    var bulan=parseInt(tahunPisah[2])
-    switch(bulan){
-        case 01 : {
+var input = ["0001", "Roman Alamsyah", "Bandar Lampung", "21/05/1989", "Membaca"]; 
+
+//["0001", "Roman Alamsyah Elsharawy", "Provinsi Bandar Lampung", "21/05/1989", "Pria", "SMA Internasional Metro"] 
+
+var arrInput;
+var tanggalLahir;
+var bulan;
+var tanggalSort;
+var tanggalan;
+var namaMax;
+
+function dataHandling2(arrInput) {
+    arrInput.splice(1,1, 'Roman Alamsyah Elsharawy');
+    arrInput.splice(2, 1, 'Provinsi Bandar Lampung');
+    arrInput.splice(4,1);
+    arrInput.splice(4, 2, 'Pria', 'SMA Internasional Metro');
+    console.log(input);
+    
+    tanggalLahir = arrInput[3].split('/');
+    console.log(gantiBulan(tanggalLahir));
+    var tanggalSort = tanggalLahir;
+    console.log(sortTanggal(tanggalSort));
+    console.log(tanggalLahir.join('-'));
+    
+    namaMax = arrInput[1];
+    console.log(namaMax.slice(0,15));
+}
+
+function gantiBulan(bulan) {
+  switch(bulan[1]) {
+    case 01 : {
             console.log('Januari');
           break; }
         case 02: {
@@ -49,15 +67,12 @@ function dataHandling2(parameter) {
         default:{
           console.log('tidak ada bulan, masukan format yang sesuai!');}
       }
-      console.log(tahunn);
-      console.log(tahunPisah.join("-"));
-      return parameter[1];
+  console.log(bulan[1]);
 }
 
-var parameter = ["0001", "Roman Alamsyah", "Bandar Lampung", "21/05/1989", "Membaca",];
-console.log(dataHandling2(parameter));
+function sortTanggal(tanggalan) {
+    var result = tanggalan.sort(function(value1, value2) { return Number(value1)<Number(value2)});
+    return result;
+}
 
-
-// //contoh output
-// ["0001", "Roman Alamsyah", "Bandar Lampung", "21/05/1989", "Membaca"]
-// ["0001", "Roman Alamsyah Elsharawy", "Provinsi Bandar Lampung", "21/05/1989", "Pria", "SMA Internasional Metro"]
+dataHandling2(input);
